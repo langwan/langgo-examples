ba95fe5cb7c6e1cdc5a001f3032bfeeeeddab36d
langwanluo <langwnaluo@gmail.com>
2022-12-07 02:11:13 +0800

    add tempfile

---
+++ 0.5.x/Components/TempFile/Simple/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/langwan/langgo"
+	"github.com/langwan/langgo/components/tempfile"
+)
+
+func main() {
+	langgo.Run(&tempfile.Instance{Base: "tmp"})
+	filename, err := tempfile.Get().CreateFile([]byte("langgo"), 0644)
+	if err != nil {
+		panic(err)
+		return
+	}
+	data, err := tempfile.Get().ReadFile(filename, false)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Println(string(data))
+	tempfile.Get().RemoveFile(filename)
+}
